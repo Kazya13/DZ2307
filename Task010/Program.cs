01af40ebb3cf5108090ae4b7832a022bd9b08861
Kazya13 <tatyana.a.kim@gmail.com>
2023-07-25 23:50:05 +1100

    Resheno

---
+++ Task010/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10. Напишите программу, которая 
+// принимает на вход положительное трёхзначное число и
+//  на выходе показывает вторую цифру этого числа.
+// 
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+vvod: Console.WriteLine("Введите 3значное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number *= -1;
+}
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не 3 значное число, введите 3 значное");
+    goto vvod;
+}
+int secondDigit = (number / 10) % 10;
+
+Console.WriteLine("Вторая цифра 3значного числа: " + secondDigit);
+
